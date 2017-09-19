3d663af91e6d3f2601260365cccdf77265def81b
3lfind@gmail.com <3lfind@gmail.com>
2017-09-19 18:28:59 -0400

    test

--- week2/madlibs.js
+++ week2/madlibs.js
@@ -1,3 +1,4 @@
+    var submit = document.getElementById("submit");
 submit.onclick = function(){
     var name = document.getElementById("name");
     var place = document.getElementById("place");
@@ -5,8 +6,14 @@ submit.onclick = function(){
     var number = document.getElementById("number");
     var adjective = document.getElementById("adjective");
     var date = document.getElementById("date");
-    var submit = document.getElementById("submit");
-    var note = document.getElementById("note");
+   // var note = document.getElementById("note");
     
-    note.innerText = name.value +" could not go to "+place.value+" today. She/he had a horrible " + illness.value + " because she/he drank "+number.value+" cups of coffee. She/he needs to see Doctor "+ adjective.value + " today. Please accept this written notification of my abesence on " + date.value+"."
+    if(number.value){
+         note.innerText = name.value +" could not go to "+place.value+" today. She/he had a horrible " + illness.value + " because she/he drank "+number.value+" cups of coffee. She/he needs to see Doctor "+ adjective.value + " today. Please accept this written notification of my abesence on " + date.value+".";
+    }else{
+         note.innerText ="Please enter a number!";
+        
+    }
+    
+  
 }
