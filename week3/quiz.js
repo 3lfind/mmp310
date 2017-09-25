2e335641743095386093b3ec9ffc70aa44cc0dfe
3lfind@gmail.com <3lfind@gmail.com>
2017-09-25 03:38:16 -0400

    math quiz

---
+++ week3/quiz.js
@@ -0,0 +1,26 @@
+var submit = document.getElementById('submit');
+submit.onclick=function(){
+	var inputs = document.getElementById('questions').getElementsByTagName('input');
+	var num =0;
+	if(inputs[0].value=="7"){
+		num++;
+	}else{
+		hint1.innerText ="Hint: 12-5";
+	}
+	if(inputs[1].value =="false"){
+		num++;
+	}else{
+		hint2.innerText ="Hint: -2^3=-8; 2^3=8";
+	}
+	if(inputs[2].value =="11"){
+		num++;
+	}else{
+		hint3.innerText ="Hint: one dozen is 12 eggs";
+	}
+	if(inputs[3].value =="false"){
+		num++;
+	}else{
+		hint4.innerText ="Hint: Not just count the small squares";
+	}
+	results.innerText ="your score is "+num*25;
+}
