7b1fcf307836dd1fbaf647c68dca1bf76e5f9542
3lfind@gmail.com <3lfind@gmail.com>
2017-10-02 13:39:07 -0400

    week4

--- week4/ball.js
+++ week4/ball.js
@@ -1,9 +1,10 @@
 var answers=["Shake again","Be careful","Admit Mistakes","She/He will refuse","Go ahead","Absolutely","Luck is with you","Today is the day","No!No!No!","lol" ];
-var submit=document.getElementById("submit");
-submit.onclick =function(){
+var answer=document.getElementById("answer");
+answer.onclick =function(){
 	var question=document.getElementById("question");
 	var answer =answers[Math.floor(Math.random()*answers.length)];
-	
-	response.innerText=question.value+answer;
+
+	response.innerText= question.value +" answer:"+answer;
 	
 }
+
